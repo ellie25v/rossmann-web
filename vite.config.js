aa5c72b7adda6159879315e68df9933a2082dc87
ellie25v <yelizarieva.ellie@gmail.com>
2025-05-26 08:19:08 +0200

    logo and title added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rossmann-web/',
   plugins: [react()],
   server: {
     proxy: {
@@ -11,5 +12,4 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-  },
-});
+  
